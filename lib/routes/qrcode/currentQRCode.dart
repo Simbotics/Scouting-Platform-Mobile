345c2ff1336eae0b4949cc218112abea186ea824
IanTapply22 <tapplyi@students.dsbn.org>
2023-04-10 17:20:15 -0400

    Changed formatting of comments section and is now displaying vital match details Moved QR code to separate page for reliable scanning Closes gH-16.

--- lib/routes/qrcode/currentQRCode.dart
+++ lib/routes/qrcode/currentQRCode.dart
@@ -2,7 +2,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:scouting_platform/main.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:scouting_platform/ui/style/style.dart';
 import 'package:scouting_platform/utils/data/autoData.dart';
@@ -18,39 +17,30 @@ class CurrentQRCode extends StatelessWidget {
   Widget build(BuildContext context) {
     Codec<String, String> stringToBase64 = utf8.fuse(base64);
     return Scaffold(
-      backgroundColor: getBackgroundColour(),
-      appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(40.0),
-          child: AppBar(
-            backgroundColor: AppStyle.textInputColor,
-            title: Text(
-              title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontFamily: 'Futura'),
-            ),
-          )),
-      body: Center(
-        child: TextButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const HomeScreen();
-              }));
-            },
-            child: Center(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                //width: MediaQuery.of(context).size.width,
-                child: QrImage(
-                  // Access variables through widget
-                  data: stringToBase64.encode(
-                      "${int.tryParse(TeamAndMatchData.teamNumberController.text) ?? 0}~${int.tryParse(TeamAndMatchData.matchNumberController.text) ?? 0}~${TeamAndMatchData.initialsController.text}~${TeamAndMatchData.teamAlliance}~${int.parse(AutoData.autoLowController.text)}~${int.parse(AutoData.autoMidController.text)}~${int.parse(AutoData.autoHighController.text)}~${int.parse(AutoData.autoMissedController.text)}~${AutoData.currentAutoMobility}~${AutoData.currentAutoBalanceState}~${int.tryParse(TeleopData.autoBalanceTimeController.text) ?? 0}~${int.parse(TeleopData.teleopConeLowController.text)}~${int.parse(TeleopData.teleopConeMidController.text)}~${int.parse(TeleopData.teleopConeHighController.text)}~${int.parse(TeleopData.teleopConeMissedController.text)}~${int.parse(TeleopData.teleopConeDroppedController.text)}~${int.parse(TeleopData.teleopCubeLowController.text)}~${int.parse(TeleopData.teleopCubeMidController.text)}~${int.parse(TeleopData.teleopCubeHighController.text)}~${int.parse(TeleopData.teleopCubeMissedController.text)}~${int.parse(TeleopData.teleopCubeDroppedController.text)}~${TeleopData.currentTeleopBalanceState}~${int.tryParse(TeleopData.teleopBalanceTimeController.text) ?? 0}~${CommentsData.autoCommentsController.text.replaceAll("\n", "")}~${CommentsData.preferenceCommentsController.text.replaceAll("\n", "")}~${CommentsData.otherCommentsController.text.replaceAll("\n", "")}"),
-                  backgroundColor: Colors.white,
-                  version: QrVersions.auto,
-                ),
+        backgroundColor: getBackgroundColour(),
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(40.0),
+            child: AppBar(
+              backgroundColor: AppStyle.textInputColor,
+              title: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontFamily: 'Futura'),
               ),
             )),
-      ),
-    );
+        body: Center(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            //width: MediaQuery.of(context).size.width,
+            child: QrImage(
+              // Access variables through widget
+              data: stringToBase64.encode(
+                  "${int.tryParse(TeamAndMatchData.teamNumberController.text) ?? 0}~${int.tryParse(TeamAndMatchData.matchNumberController.text) ?? 0}~${TeamAndMatchData.initialsController.text}~${TeamAndMatchData.teamAlliance}~${int.parse(AutoData.autoLowController.text)}~${int.parse(AutoData.autoMidController.text)}~${int.parse(AutoData.autoHighController.text)}~${int.parse(AutoData.autoMissedController.text)}~${AutoData.currentAutoMobility}~${AutoData.currentAutoBalanceState}~${int.tryParse(TeleopData.autoBalanceTimeController.text) ?? 0}~${int.parse(TeleopData.teleopConeLowController.text)}~${int.parse(TeleopData.teleopConeMidController.text)}~${int.parse(TeleopData.teleopConeHighController.text)}~${int.parse(TeleopData.teleopConeMissedController.text)}~${int.parse(TeleopData.teleopConeDroppedController.text)}~${int.parse(TeleopData.teleopCubeLowController.text)}~${int.parse(TeleopData.teleopCubeMidController.text)}~${int.parse(TeleopData.teleopCubeHighController.text)}~${int.parse(TeleopData.teleopCubeMissedController.text)}~${int.parse(TeleopData.teleopCubeDroppedController.text)}~${TeleopData.currentTeleopBalanceState}~${int.tryParse(TeleopData.teleopBalanceTimeController.text) ?? 0}~${CommentsData.autoCommentsController.text.replaceAll("\n", "")}~${CommentsData.preferenceCommentsController.text.replaceAll("\n", "")}~${CommentsData.otherCommentsController.text.replaceAll("\n", "")}"),
+              backgroundColor: Colors.white,
+              version: QrVersions.auto,
+            ),
+          ),
+        ));
   }
 
   static String formatTime(int seconds) {
--- lib/sections/comments.dart
+++ lib/sections/comments.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:scouting_platform/utils/data/autoData.dart';
+import 'package:scouting_platform/routes/qrcode/currentQRCode.dart';
+import 'package:scouting_platform/textStyles/header.dart';
 import 'package:scouting_platform/utils/data/commentsData.dart';
 import 'package:scouting_platform/utils/data/teamAndMatchData.dart';
-import 'package:scouting_platform/utils/data/teleopData.dart';
 import 'package:screen_brightness/screen_brightness.dart';
 
 import '../../ui/style/style.dart';
@@ -26,87 +24,98 @@ class CommentsSection extends StatefulWidget {
 class _CommentsSectionState extends State<CommentsSection> {
   Future<Image>? image;
 
-  static Codec<String, String> stringToBase64 = utf8.fuse(base64);
-
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
         Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          //mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            TextInputField(
-              controller: CommentsData.autoCommentsController,
-              margin: const EdgeInsets.only(left: 20.0),
-              width: 400.0,
-              height: 100.0,
-              onChanged: (value) {},
-              textAlign: TextAlign.justify,
-              hintText: "Auto (Starting position? Routine? etc)",
-              maxLines: 3,
-            ),
-            TextInputField(
-              controller: CommentsData.preferenceCommentsController,
-              margin: const EdgeInsets.only(left: 20.0),
-              width: 400.0,
-              height: 100.0,
-              onChanged: (value) {},
-              textAlign: TextAlign.justify,
-              hintText: "Preference (Cubes/Cones, Pickup Location, etc)",
-              maxLines: 3,
-            ),
-            TextInputField(
-              controller: CommentsData.otherCommentsController,
-              margin: const EdgeInsets.only(left: 20.0),
-              width: 400.0,
-              height: 100.0,
-              onChanged: (value) {},
-              textAlign: TextAlign.justify,
-              hintText: "Other (Anything else important)",
-              maxLines: 3,
-            ),
-            Container(
-                padding: const EdgeInsets.only(left: 20.0),
-                height: 47.0,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppStyle
-                        .textInputColorLight, // Set the background color here
-                  ),
-                  onPressed: () async {
-                    setState(() {
-                      CommentsSection.qrIsVisible = true;
-                    });
-                    setBrightness(1.0);
-                    // Navigator.push(context,
-                    //     MaterialPageRoute(builder: (context) {
-                    //   return const CurrentQRCode(title: "Current QR Code");
-                    // }));
-                  },
-                  child: const Text(
-                    'Update QR Code',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                )),
+            Row(children: [
+              TextInputField(
+                controller: CommentsData.autoCommentsController,
+                margin: const EdgeInsets.only(left: 20.0),
+                width: 600.0,
+                height: 100.0,
+                onChanged: (value) {},
+                textAlign: TextAlign.justify,
+                hintText: "Auto (Starting position? Routine? etc)",
+                maxLines: 3,
+              ),
+            ]),
+            Row(children: [
+              TextInputField(
+                controller: CommentsData.preferenceCommentsController,
+                margin: const EdgeInsets.only(left: 20.0),
+                width: 600.0,
+                height: 100.0,
+                onChanged: (value) {},
+                textAlign: TextAlign.justify,
+                hintText: "Preference (Cubes/Cones, Pickup Location, etc)",
+                maxLines: 3,
+              ),
+            ]),
+            Row(children: [
+              TextInputField(
+                controller: CommentsData.otherCommentsController,
+                margin: const EdgeInsets.only(left: 20.0),
+                width: 600.0,
+                height: 125.0,
+                onChanged: (value) {},
+                textAlign: TextAlign.justify,
+                hintText: "Other (Anything else important)",
+                maxLines: 5,
+              ),
+            ]),
+            Row(children: [
+              Container(
+                  padding: const EdgeInsets.only(right: 2),
+                  height: 47.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppStyle
+                          .textInputColorLight, // Set the background color here
+                    ),
+                    onPressed: () async {
+                      // setState(() {
+                      //   CommentsSection.qrIsVisible = true;
+                      // });
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return const CurrentQRCode(title: "Current QR Code");
+                      }));
+                      setBrightness(1.0);
+                    },
+                    child: const Text(
+                      'Go To QR Code',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  )),
+              HeaderStyle(
+                text:
+                    "   Team Number: ${TeamAndMatchData.teamNumberController.text}  |  Match Number: ${TeamAndMatchData.matchNumberController.text}",
+                padding: const EdgeInsets.only(left: 0.0),
+              )
+            ])
           ],
         ),
-        Visibility(
-          visible: CommentsSection.qrIsVisible,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 120.0, bottom: 60.0),
-            child: SizedBox(
-              width:
-                  300.0, // set the width of the container as per your requirement
-              child: QrImage(
-                // Access variables through widget
-                data: stringToBase64.encode(
-                    "${int.tryParse(TeamAndMatchData.teamNumberController.text) ?? 0}~${int.tryParse(TeamAndMatchData.matchNumberController.text) ?? 0}~${TeamAndMatchData.initialsController.text}~${TeamAndMatchData.teamAlliance}~${int.parse(AutoData.autoLowController.text)}~${int.parse(AutoData.autoMidController.text)}~${int.parse(AutoData.autoHighController.text)}~${int.parse(AutoData.autoMissedController.text)}~${AutoData.currentAutoMobility}~${AutoData.currentAutoBalanceState}~${int.tryParse(TeleopData.autoBalanceTimeController.text) ?? 0}~${int.parse(TeleopData.teleopConeLowController.text)}~${int.parse(TeleopData.teleopConeMidController.text)}~${int.parse(TeleopData.teleopConeHighController.text)}~${int.parse(TeleopData.teleopConeMissedController.text)}~${int.parse(TeleopData.teleopConeDroppedController.text)}~${int.parse(TeleopData.teleopCubeLowController.text)}~${int.parse(TeleopData.teleopCubeMidController.text)}~${int.parse(TeleopData.teleopCubeHighController.text)}~${int.parse(TeleopData.teleopCubeMissedController.text)}~${int.parse(TeleopData.teleopCubeDroppedController.text)}~${TeleopData.currentTeleopBalanceState}~${int.tryParse(TeleopData.teleopBalanceTimeController.text) ?? 0}~${CommentsData.autoCommentsController.text.replaceAll("\n", "")}~${CommentsData.preferenceCommentsController.text.replaceAll("\n", "")}~${CommentsData.otherCommentsController.text.replaceAll("\n", "")}"),
-                backgroundColor: Colors.white,
-                version: QrVersions.auto,
-              ),
-            ),
-          ),
-        ),
+        // Visibility(
+        //   visible: CommentsSection.qrIsVisible,
+        //   child: Padding(
+        //     padding: const EdgeInsets.only(left: 120.0, bottom: 60.0),
+        //     child: SizedBox(
+        //       width:
+        //           300.0, // set the width of the container as per your requirement
+        //       child: QrImage(
+        //         // Access variables through widget
+        //         data: stringToBase64.encode(
+        //             "${int.tryParse(TeamAndMatchData.teamNumberController.text) ?? 0}~${int.tryParse(TeamAndMatchData.matchNumberController.text) ?? 0}~${TeamAndMatchData.initialsController.text}~${TeamAndMatchData.teamAlliance}~${int.parse(AutoData.autoLowController.text)}~${int.parse(AutoData.autoMidController.text)}~${int.parse(AutoData.autoHighController.text)}~${int.parse(AutoData.autoMissedController.text)}~${AutoData.currentAutoMobility}~${AutoData.currentAutoBalanceState}~${int.tryParse(TeleopData.autoBalanceTimeController.text) ?? 0}~${int.parse(TeleopData.teleopConeLowController.text)}~${int.parse(TeleopData.teleopConeMidController.text)}~${int.parse(TeleopData.teleopConeHighController.text)}~${int.parse(TeleopData.teleopConeMissedController.text)}~${int.parse(TeleopData.teleopConeDroppedController.text)}~${int.parse(TeleopData.teleopCubeLowController.text)}~${int.parse(TeleopData.teleopCubeMidController.text)}~${int.parse(TeleopData.teleopCubeHighController.text)}~${int.parse(TeleopData.teleopCubeMissedController.text)}~${int.parse(TeleopData.teleopCubeDroppedController.text)}~${TeleopData.currentTeleopBalanceState}~${int.tryParse(TeleopData.teleopBalanceTimeController.text) ?? 0}~${CommentsData.autoCommentsController.text.replaceAll("\n", "")}~${CommentsData.preferenceCommentsController.text.replaceAll("\n", "")}~${CommentsData.otherCommentsController.text.replaceAll("\n", "")}"),
+        //         backgroundColor: Colors.white,
+        //         version: QrVersions.auto,
+        //       ),
+        //     ),
+        //   ),
+        // ),
       ],
     );
   }
