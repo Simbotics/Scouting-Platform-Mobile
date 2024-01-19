ede942d410e886c81d9f72581456e6915fcbe0ea
IanTapply22 <contact@ian-tapply.me>
2024-01-19 15:45:43 -0500

    Made stopwatch stateful

--- lib/builders/fields/stopwatchButton.dart
+++ lib/builders/fields/stopwatchButton.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 /// THIS CLASS IS DEPRECATED AND IS NO LONGER SUPPORTED FOR USE
 /// Please refer to previous years of for usage of this class
 /// NO SUPPORT WILL BE PROVIDED FOR THIS CLASSES USAGE
-class StopwatchButton extends StatelessWidget {
+class StopwatchButton extends StatefulWidget {
   final TextEditingController state;
   final Stopwatch timer;
 
@@ -15,20 +15,11 @@ class StopwatchButton extends StatelessWidget {
     required this.timer,
   }) : super(key: key);
 
-  String returnFormattedText() {
-    int milli = timer.elapsed.inMilliseconds;
+    @override
+  State<StopwatchButton> createState() => _StopwatchButtonState();
+}
 
-    if(milli == 0){
-      return "Start Timer";
-    }
- 
-    String milliseconds = (milli % 1000).toString().padLeft(1, "0"); 
-    String seconds = ((milli ~/ 1000) % 60).toString().padLeft(2, "0"); 
-    String minutes = ((milli ~/ 1000) ~/ 60).toString().padLeft(1, "0"); 
- 
-    return "$minutes:$seconds:$milliseconds";
-  }
-
+class _StopwatchButtonState extends State<StopwatchButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -40,22 +31,36 @@ class StopwatchButton extends StatelessWidget {
             shape:const ContinuousRectangleBorder(),
             ),
             onPressed: () {
-              if(int.parse(state.text) == 1){
-                timer.stop();
-                state.text = "2";
-              }else if(int.parse(state.text) == 2){
-                state.text = "3";
-              }else if(int.parse(state.text) == 3){
-                timer.reset();
-                state.text = "0";
+              if(int.parse(widget.state.text) == 1){
+                widget.timer.stop();
+                widget.state.text = "2";
+              }else if(int.parse(widget.state.text) == 2){
+                widget.state.text = "3";
+              }else if(int.parse(widget.state.text) == 3){
+                widget.timer.reset();
+                widget.state.text = "0";
              }else{
-                timer.start();
-                state.text = "1";
+                widget.timer.start();
+                widget.state.text = "1";
              }
             },
         child: Text(returnFormattedText(),
             style: const TextStyle(fontSize: 16.0, fontFamily: "Helvetica", color: Colors.white)),
       ),
     );
+  }
+
+    String returnFormattedText() {
+    int milli = widget.timer.elapsed.inMilliseconds;
+
+    if(milli == 0){
+      return "Start Timer";
+    }
+ 
+    String milliseconds = (milli % 1000).toString().padLeft(1, "0"); 
+    String seconds = ((milli ~/ 1000) % 60).toString().padLeft(2, "0"); 
+    String minutes = ((milli ~/ 1000) ~/ 60).toString().padLeft(1, "0"); 
+ 
+    return "$minutes:$seconds:$milliseconds";
   }
 }
