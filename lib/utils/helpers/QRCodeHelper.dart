0c5288901dccc58f689b320c762e534fc563eb43
JasonLiang <liangja@students.dsbn.org>
2026-04-26 10:39:33 -0400

    rm extra line in qrcode

--- lib/utils/helpers/QRCodeHelper.dart
+++ lib/utils/helpers/QRCodeHelper.dart
@@ -82,7 +82,6 @@ class QrcodeHelper {
   static String computeEndgameValues() {
     return computeValues([
       parseString(EndgameValues.endGameClimb.text), // index 12
-      parseString(EndgameValues.endGameClimb.text), // index 12
     ]);
   }
 
